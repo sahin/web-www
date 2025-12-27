d371cdd64e8df1171776875c9c2679bfbda6e220
Sahin Boydas <sahin@users.noreply.github.com>
2025-12-27 01:03:52 -0600

    Add Vercel rewrites for Buffett indicators

---
+++ vercel.js
@@ -0,0 +1,12 @@
+{
+  "rewrites": [
+    {
+      "source": "/warren-buffett-indicators",
+      "destination": "https://web-buffett-indicator.vercel.app/warren-buffett-indicators"
+    },
+    {
+      "source": "/warren-buffett-indicators/:path*",
+      "destination": "https://web-buffett-indicator.vercel.app/warren-buffett-indicators/:path*"
+    }
+  ]
+}
